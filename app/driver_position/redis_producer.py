697e3afc57801f5d83be9cd3a983dc616efeb4fd
Denise Avila <denisefavila@gmail.com>
2024-12-20 11:02:49 -0300

    feat: add kafka producers

--- app/driver_position/redis_producer.py
+++ app/driver_position/redis_producer.py
@@ -1,23 +1,15 @@
 import logging
 import os
-import random
 import signal
-import uuid
-from datetime import datetime
 
 from dotenv import load_dotenv
 
+from app.driver_position.generate_data import generate_driver_position
 from app.redis_stream.redis_client import redis_client
 from app.redis_stream.redis_producer import RedisProducer, signal_handler
 
 # Load environment variables from the .env file
 load_dotenv()
-
-# Coordinates for Belo Horizonte (central area)
-BH_LAT_MIN = -20.0047113796
-BH_LAT_MAX = -19.7890619963
-BH_LON_MIN = -44.0986149944
-BH_LON_MAX = -43.860692326
 
 DRIVER_POSITION_STREAM = os.getenv("REDIS_STREAM", "driver_position_stream")
 
@@ -27,17 +19,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 shutdown_flag = False
-
-
-def generate_driver_position():
-    driver_id = uuid.uuid4()
-    """Generate a driver's position within Belo Horizonte's coordinates."""
-    return {
-        "driver_id": str(driver_id),
-        "latitude": f"{random.uniform(BH_LAT_MIN, BH_LAT_MAX):.6f}",
-        "longitude": f"{random.uniform(BH_LON_MIN, BH_LON_MAX):.6f}",
-        "timestamp": datetime.utcnow().isoformat(),
-    }
 
 
 if __name__ == "__main__":
